e01876f5bef7c94ccdce0f24f8eb82ba0140351f
melhemrizk <29739992+melhemrizk@users.noreply.github.com>
2024-10-10 22:04:39 +0300

    added the grub configuration file, the sway config file and other small changes

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,12 +1,23 @@
 # Aliases
+#zsh aliases
 alias zshrc="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
+#Vim aliases
 alias vim="nvim"
+#Navigation and listing aliases
+alias ..='cd ..'
 alias cd..="cd .."
+alias ..='cd ..'
+alias ...='cd ../..'
+alias dir='ls -l'
 alias grep="grep --color=auto"
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias ls="eza"
 alias ll="eza -lh --icons=auto"
 alias la="eza -lah --icons=auto --group-directories-first"
 alias ldot="eza -ld --group-directories-first .*"
+alias unmount='echo "Error: Try the command: umount" 1>&2; false'
 #Zypper aliases
 alias zh="zypper -h"
 alias zinfo="zypper info --requires --recommends"
@@ -14,9 +25,12 @@ alias zdup="sudo zypper ref && sudo zypper dup -y"
 alias zin="sudo zypper in"
 alias zrm="sudo zypper rm --clean-deps"
 alias zse="zypper se"
+alias zref="sudo zypper ref"
 #Updating the flatpak packages
 alias fupd="flatpak update --user -y"
 #Clearing the terminal
 alias cls="clear"
 
 alias cat="bat"
+
+alias dd="dd bs=4M status=progress"
